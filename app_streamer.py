0cb79108dcf7b4ab7e90fe1ecabb9d7b93a08f93
Supanika101 <147579561+ohure1297@users.noreply.github.com>
2025-04-23 08:53:44 +0700

    Update app_streamer.py

--- app_streamer.py
+++ app_streamer.py
@@ -26,12 +26,10 @@ with col1:
                           
                           rtc_configuration={ # Add this config
                           
-                          "IceServers": [
-                            {"urls": ["stun:stun.l.google.com:19302"],
-                            "urls": ["stun:stun1.l.google.com:19302"],
-                            "urls": ["stun:stun2.l.google.com:19302"],
-                            "urls": ["stun:stun3.l.google.com:19302"],
-                            "urls": ["stun:stun4.l.google.com:19302"],
+                          "iceServers": [
+                            {"urls": ["turns:openrelay:mtred:ca:80"],
+                            "username": "openrelayproject",
+                            "credential": "openrelayproject"
                           }
                           ]
                           },
